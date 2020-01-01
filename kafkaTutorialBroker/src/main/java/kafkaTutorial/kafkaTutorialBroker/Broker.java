0c4adddd3b24ec129c853991c80e343c6acf1cbb
admin <admin@DESKTOP-EN06J15>
2020-01-01 22:29:15 +0900

    kafka tutorial update broker

---
+++ kafkaTutorialBroker/src/main/java/kafkaTutorial/kafkaTutorialBroker/Broker.java
@@ -0,0 +1,13 @@
+package kafkaTutorial.kafkaTutorialBroker;
+
+import java.util.Properties;
+import org.apache.kafka.server.*;
+
+public class Broker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties configs = new Properties();
+	}
+
+}
